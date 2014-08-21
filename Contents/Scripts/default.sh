50501c096b65cc02f31ff719ca6c32a2af1e16bf
Andrew Schleifer <me@andrewschleifer.name>
2014-08-21 17:48:33 -0500

    Initial version

---
+++ Contents/Scripts/default.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+echo '['
+
+osascript -e "tell application \"Adium\" to get the id of every account whose status type is offline" | \
+    sed 's/, /\'$'\n/g' | \
+    while read ID; do
+        printf "tell application \"Adium\" to return (name, id, service) of account id $ID" | \
+        osascript | \
+        sed '
+            s|^|{ "title": "|;
+            s|, service |", "subtitle": "Connect to |;
+            s|, |", "actionArgument": "|;
+            s|$|", "action": "online.sh", "icon": "com.adiumX.adiumX" },|
+        '
+    done
+
+osascript -e "tell application \"Adium\" to get the id of every account whose status type is not offline" | \
+    sed 's/, /\'$'\n/g' | \
+    while read ID; do
+        printf "tell application \"Adium\" to return (name, id, service) of account id $ID" | \
+            osascript | \
+            sed '
+                s|^|{ "title": "|;
+                s|, service |", "subtitle": "Disconnect from |;
+                s|, |", "actionArgument": "|;
+                s|$|", "action": "offline.sh", "icon": "com.adiumX.adiumX" },|
+            '
+    done
+
+echo ']'
---
+++ Contents/Scripts/offline.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+osascript -e "tell application \"Adium\" to account id ${1} go offline"
